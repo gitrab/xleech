0aeccd2d0e162fc6942f255050735de9836ecdd0
cybernet2u <cybernet2u@0aa21304-ca48-640c-94ec-0df0ad2d8eba>
2010-10-13 21:03:33 +0000

    // Copyright modded
    // Have FuN
    
    // CyBerFuN .::. xList //
    // http://xleech-source.co.cc/
    // http://www.cyberfun.ro/
    // http://xList.ro/
    // http://xDnS.ro/
    // http://xLeech.in/
    // Modified By cybernet2u

--- include/validator.php
+++ include/validator.php
@@ -33,22 +33,22 @@
 // http://sourceforge.net/projects/xleech/
 // http://xleech.sourceforge.net/
 
-function validator($context){
+function validator($context) {
        global $CURUSER;
        $timestamp = time();
        $hash = hash_hmac("sha1", $CURUSER['secret'], $context.$timestamp);
        return substr($hash, 0, 20).dechex($timestamp);
 }
-function validatorForm($context){
+function validatorForm($context) {
        return "<input type=\"hidden\" name=\"validator\" value=\"".validator($context)."\"/>";
 }
 
-function validate($validator, $context, $seconds = 0){
+function validate($validator, $context, $seconds = 0) {
        global $CURUSER;
        $timestamp = hexdec(substr($validator, 20));
        if($seconds && time() > $timestamp + $seconds)
                return False;
-       $hash=substr(hash_hmac("sha1", $CURUSER['secret'], $context.$timestamp), 0, 20);
+       $hash = substr(hash_hmac("sha1", $CURUSER['secret'], $context.$timestamp), 0, 20);
        if (substr($validator, 0, 20) != $hash)
                return False;
        return True;
