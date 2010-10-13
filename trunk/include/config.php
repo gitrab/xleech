e701627d1b46c433185a7f6948fef7b30b18b180
cybernet2u <cybernet2u@0aa21304-ca48-640c-94ec-0df0ad2d8eba>
2010-10-13 20:59:27 +0000

    // Copyright modded
    // Have FuN
    
    // CyBerFuN .::. xList //
    // http://xleech-source.co.cc/
    // http://www.cyberfun.ro/
    // http://xList.ro/
    // http://xDnS.ro/
    // http://xLeech.in/
    // Modified By cybernet2u

--- trunk/include/config.php
+++ trunk/include/config.php
@@ -16,6 +16,23 @@
 |   $URL$
 +------------------------------------------------
 */
+
+// CyBerFuN.ro & xList.ro & xLeech.in & xDNS.ro
+
+// xLeech .::. config
+// http://www.cyberfun.ro/
+// http://xList.ro/
+// http://xDnS.ro/
+// http://xLeech.in/
+// Modified By cybernet2u
+
+// xLeech v1.2
+
+// http://xleech-source.co.cc/
+// https://xleech.svn.sourceforge.net/svnroot/xleech
+// http://sourceforge.net/projects/xleech/
+// http://xleech.sourceforge.net/
+
 error_reporting(E_ALL);
 
 define('SQL_DEBUG', 2);
