d17b321e9b539f9e47c9e0c43ce7f27a747f8784
cybernet2u <cybernet2u@0aa21304-ca48-640c-94ec-0df0ad2d8eba>
2010-10-13 20:45:39 +0000

    // Copyright modded
    // Have FuN
    
    // CyBerFuN .::. xList //
    // http://xleech-source.co.cc/
    // http://www.cyberfun.ro/
    // http://xList.ro/
    // http://xDnS.ro/
    // http://xLeech.in/
    // Modified By cybernet2u

--- takefilesearch.php
+++ takefilesearch.php
@@ -16,12 +16,29 @@
 |   $URL$
 +------------------------------------------------
 */
+
+// CyBerFuN.ro & xList.ro & xLeech.in & xDNS.ro
+
+// xLeech .::. take file search
+// http://www.cyberfun.ro/
+// http://xList.ro/
+// http://xDnS.ro/
+// http://xLeech.in/
+// Modified By cybernet2u
+
+// xLeech v1.2
+
+// http://xleech-source.co.cc/
+// https://xleech.svn.sourceforge.net/svnroot/xleech
+// http://sourceforge.net/projects/xleech/
+// http://xleech.sourceforge.net/
+
 ob_start('ob_gzhandler');
 
-require_once 'include/bittorrent.php';
-require_once 'include/user_functions.php';
-require_once "include/html_functions.php";
-//require_once "include/pager_functions.php";
+require_once ('include/bittorrent.php');
+require_once ('include/user_functions.php');
+require_once ("include/html_functions.php");
+// require_once ("include/pager_functions.php");
 
 dbconn(false);
 
