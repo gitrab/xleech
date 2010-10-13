ef388f217fe9328a7adef6eb00be81f838f7e1c0
cybernet2u <cybernet2u@0aa21304-ca48-640c-94ec-0df0ad2d8eba>
2010-10-13 20:54:43 +0000

    // Copyright modded
    // Have FuN
    
    // CyBerFuN .::. xList //
    // http://xleech-source.co.cc/
    // http://www.cyberfun.ro/
    // http://xList.ro/
    // http://xDnS.ro/
    // http://xLeech.in/
    // Modified By cybernet2u

--- useragreement.php
+++ useragreement.php
@@ -16,9 +16,26 @@
 |   $URL$
 +------------------------------------------------
 */
-require "include/bittorrent.php";
-require_once "include/html_functions.php";
-require_once "include/user_functions.php";
+
+// CyBerFuN.ro & xList.ro & xLeech.in & xDNS.ro
+
+// xLeech .::. user agreement
+// http://www.cyberfun.ro/
+// http://xList.ro/
+// http://xDnS.ro/
+// http://xLeech.in/
+// Modified By cybernet2u
+
+// xLeech v1.2
+
+// http://xleech-source.co.cc/
+// https://xleech.svn.sourceforge.net/svnroot/xleech
+// http://sourceforge.net/projects/xleech/
+// http://xleech.sourceforge.net/
+
+require ("include/bittorrent.php");
+require_once ("include/html_functions.php");
+require_once ("include/user_functions.php");
 
 dbconn();
     
@@ -34,4 +51,5 @@ dbconn();
     $HTMLOUT .= end_frame();
     $HTMLOUT .= end_main_frame();
     print stdhead("{$lang['stdhead_usragrmnt']}") . $HTMLOUT . stdfoot();
+
 ?>
--- userdetails.php
+++ userdetails.php
@@ -16,6 +16,23 @@
 |   $URL$
 +------------------------------------------------
 */
+
+// CyBerFuN.ro & xList.ro & xLeech.in & xDNS.ro
+
+// xLeech .::. user details
+// http://www.cyberfun.ro/
+// http://xList.ro/
+// http://xDnS.ro/
+// http://xLeech.in/
+// Modified By cybernet2u
+
+// xLeech v1.2
+
+// http://xleech-source.co.cc/
+// https://xleech.svn.sourceforge.net/svnroot/xleech
+// http://sourceforge.net/projects/xleech/
+// http://xleech.sourceforge.net/
+
 require_once ("include/bittorrent.php");
 require_once ("include/user_functions.php");
 require_once ("include/page_verify.php");
