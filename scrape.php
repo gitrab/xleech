a4cb155a93c70e4bf7d482d584ef3c49037b9734
cybernet2u <cybernet2u@0aa21304-ca48-640c-94ec-0df0ad2d8eba>
2010-10-13 18:45:43 +0000

    // Copyright modded and private patch applied
    // Have FuN
    
    // CyBerFuN .::. xList //
    // http://xleech-source.co.cc/
    // http://www.cyberfun.ro/
    // http://xList.ro/
    // http://xDnS.ro/
    // http://xLeech.in/
    // Modified By cybernet2u

--- scrape.php
+++ scrape.php
@@ -16,7 +16,27 @@
 |   $URL: https://tbdevnet.svn.sourceforge.net/svnroot/tbdevnet/trunk/TB/scrape.php $
 +------------------------------------------------
 */
-require_once("include/config.php");
+
+// CyBerFuN.ro & xList.ro & xLeech.in & xDNS.ro
+
+// xLeech .::. scrape
+// http://www.cyberfun.ro/
+// http://xList.ro/
+// http://xDnS.ro/
+// http://xLeech.in/
+// Modified By cybernet2u
+
+// xLeech v1.2
+
+// http://xleech-source.co.cc/
+// https://xleech.svn.sourceforge.net/svnroot/xleech
+// http://sourceforge.net/projects/xleech/
+// http://xleech.sourceforge.net/
+
+$TBDEV['mysql_host'] = "cancer";
+$TBDEV['mysql_user'] = "you_wish";
+$TBDEV['mysql_pass'] = "you_wish";
+$TBDEV['mysql_db']   = "cancer_live";
 
     if (!@mysql_connect($TBDEV['mysql_host'], $TBDEV['mysql_user'], $TBDEV['mysql_pass']))
     {
