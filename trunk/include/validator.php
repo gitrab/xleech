bdcb4d569d5e8ad11fbadebd4e24f3759ca6a62a
cybernet2u <cybernet2u@0aa21304-ca48-640c-94ec-0df0ad2d8eba>
2010-10-13 21:03:05 +0000

    // Copyright modded
    // Have FuN
    
    // CyBerFuN .::. xList //
    // http://xleech-source.co.cc/
    // http://www.cyberfun.ro/
    // http://xList.ro/
    // http://xDnS.ro/
    // http://xLeech.in/
    // Modified By cybernet2u

--- trunk/include/validator.php
+++ trunk/include/validator.php
@@ -1,4 +1,38 @@
 <?php
+/*
++------------------------------------------------
+|   TBDev.net BitTorrent Tracker PHP
+|   =============================================
+|   by CoLdFuSiOn
+|   (c) 2003 - 2009 TBDev.Net
+|   http://www.tbdev.net
+|   =============================================
+|   svn: http://sourceforge.net/projects/tbdevnet/
+|   Licence Info: GPL
++------------------------------------------------
+|   $Date$
+|   $Revision$
+|   $Author$
+|   $URL$
++------------------------------------------------
+*/
+
+// CyBerFuN.ro & xList.ro & xLeech.in & xDNS.ro
+
+// xLeech .::. validator
+// http://www.cyberfun.ro/
+// http://xList.ro/
+// http://xDnS.ro/
+// http://xLeech.in/
+// Modified By cybernet2u
+
+// xLeech v1.2
+
+// http://xleech-source.co.cc/
+// https://xleech.svn.sourceforge.net/svnroot/xleech
+// http://sourceforge.net/projects/xleech/
+// http://xleech.sourceforge.net/
+
 function validator($context){
        global $CURUSER;
        $timestamp = time();
